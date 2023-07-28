ed25db4dc2ca6be461ceab999e440aac96527125
Valurauta <wncjf2000@naver.com>
2023-07-28 23:54:21 +0900

    UPDATE 07/28

--- app.py
+++ app.py
@@ -13,10 +13,8 @@ df = pd.read_csv('DB/books.csv')
 df['판매가'] = df['판매가'].str.replace("원", "").str.replace(",", "").astype(int)
 # 판매 가격 점수를 정규화
 df['판매가 점수'] = 1 - (df['판매가'] - df['판매가'].min()) / (df['판매가'].max() - df['판매가'].min())
-
 # 순위 점수를 정규화
 df['순위 점수'] = 1 - (df['순위'] - df['순위'].min()) / (df['순위'].max() - df['순위'].min())
-
 # 판매가 점수와 순위 점수를 결합한 선호도 점수를 생성, 직관적으로 가중치 부여
 df['선호도 점수'] = df['판매가 점수'] * 0.1 + df['순위 점수'] * 0.9 
 
@@ -33,9 +31,11 @@ df['설명'] = df['설명'].fillna('')
 def recommend_books(df, title, cosine_sim=cosine_sim):
     idx = indices[title]
     sim_scores = list(enumerate(cosine_sim[idx]))
-    # sim_scores의 설정을 통해 리스트의 첫 번째 요소로 설정 
-    sim_scores = sorted(sim_scores, key=lambda x: np.max(x[1]), reverse=True)  
-    sim_scores = sim_scores[1:6]  # The most similar 5 books
+
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # 상위 10개가 아닌 상위 50개를 구하고 그 중 가장 '선호도 점수'가 높은 책 5권을 추천한다.
+    sim_scores = sim_scores[1:51]  
     book_indices = [i[0] for i in sim_scores]
     
     # 사용자가 선택한 책과 유사한 책들을 찾고, 해당 책들의 선호도 점수를 높임
@@ -49,18 +49,17 @@ for i, genre in enumerate(genres, 1):
 genre_no = int(input("번호로 선호장르를 선택해 주세요: "))
 user_genre = genres[genre_no-1]
 
-# 사용자가 선택한 장르를 기반으로 가장 높은 선호도 점수를 가진 책 5권 찾기
+# 사용자가 선택한 장르를 기반으로 랜덤하게 책 5권 추출
 genre_df = df[df['관리분류'] == user_genre]
-top_books = genre_df.nlargest(5, '선호도 점수')['상품명'].tolist()
-for i, bookname in enumerate(top_books, 1):
-    print(f"{i}: {bookname}")
-
+selected_books = genre_df.sample(n=5)
+for i, row in selected_books.iterrows():
+    print(f"{i}: {row['상품명']}")
 selected_books_no = input("번호로 선호하는 책을 골라주세요(여러 개 선택 가능하며, 쉼표로 구분): ").split(",")
-selected_books = [top_books[int(no)-1] for no in selected_books_no]
+selected_books = [selected_books.loc[int(no), '상품명'] for no in selected_books_no]  # idx를 그대로 사용
 
 for book in selected_books:
     recommend_books(df, book.strip())  # 사용자가 선택한 책과 유사한 책들을 찾고, 해당 책들의 선호도 점수를 높임
 
 # 최종적으로 가장 높은 선호도 점수를 가진 책 5권 추천
-recommendations = genre_df.nlargest(5, '선호도 점수')
-print("추천된 책 목록: ", recommendations['상품명'].tolist())
+recommendations = df.nlargest(5, '선호도 점수')
+print("추천된 책 목록: ", recommendations[['상품명', '선호도 점수']])
