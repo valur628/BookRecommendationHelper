8e295f0e53a98f87dd4492178a71709628218a62
Valurauta <wncjf2000@naver.com>
2023-08-24 17:23:40 +0900

    08/24 UPDATE

--- book_recommendation/model.py
+++ book_recommendation/model.py
@@ -2,21 +2,26 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+import os
 
 class State:
-    def __init__(self, genre, names, mid_genre, rec_model):
+    def __init__(self, genre, names, rec_model):
         self.genre = genre
         self.names = names
-        self.mid_genre = mid_genre
         self.rec_model = rec_model
 
 class bookrec:
-    def __init__(self):
-        df = pd.read_csv('data/books.csv')
+    def __init__(self, mid_genre):
+        for file in os.listdir('data'):
+            if mid_genre in file:
+                df = pd.read_csv(f'data/{file}')
+                break
+
         df['판매가'] = df['판매가'].str.replace("원", "").str.replace(",", "").astype(int)
         df['판매가 점수'] = 1 - (df['판매가'] - df['판매가'].min()) / (df['판매가'].max() - df['판매가'].min())
         df['중위 장르'] = df['관리분류'].apply(lambda x: x.split('-')[1])
         df['하위 장르'] = df['관리분류'].apply(lambda x: x.split('-')[2])
+
         self.tf = TfidfVectorizer(stop_words='english')
         df['설명'] = df['설명'].fillna('')
         tfidf_matrix = self.tf.fit_transform(df['설명'])
--- book_recommendation/routes.py
+++ book_recommendation/routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request
-from book_recommendation import create_app
+from flask import Blueprint, render_template, request, flash, g
+from .model import State, bookrec
 import random
 
 bp = Blueprint('main', __name__)
@@ -8,14 +8,31 @@ def random_books(state, num=10):
     return random.sample(state.names, num)
 
 @bp.route('/', methods=['GET', 'POST'])
-def main_page():
-    state = create_app().state
+def select_genre():
+    if 'mid_genres' not in g:
+        from . import create_app
+        g.mid_genres = create_app().mid_genres
+    mid_genres = g.mid_genres
+
+    if request.method == 'POST':
+        mid_genre = request.form.get("mid_genre")
+        if mid_genre:
+            new_rec = bookrec(mid_genre)
+            g.state = State(new_rec.genre, new_rec.names, new_rec)
+            return render_template('books.html', genre=g.state.genre, names=random_books(g.state))
+    return render_template('index.html', mid_genres=mid_genres)
+
+@bp.route('/books', methods=['GET', 'POST'])
+def select_books():
+    state = g.get('state')
     if request.method == 'POST':
         genre = request.form.get("genre")
         selected_books = request.form.getlist("books")
-        try:
+        if genre and selected_books:
             recommendations = state.rec_model.book_recommend(genre, selected_books)
-        except Exception as e:
-            recommendations = [str(e)]
-        return render_template('index.html', genre=state.genre, names=random_books(state), recommendations=recommendations)
-    return render_template('index.html', genre=state.genre, names=random_books(state))
+        else:
+            recommendations = []
+            flash('하위 장르와 책 선택은 필수입니다.')
+    else:
+        recommendations = []
+    return render_template('books.html', genre=state.genre, names=random_books(state), recommendations=recommendations)
